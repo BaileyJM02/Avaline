053e597f5609b90643937e4cbdccb8c279514a4e
Bailey Matthews <baileyjmatthews@gmail.com>
2018-05-31 17:34:09 +0100

    Major server updates
    Added:
    - New CSS
    - New Layout
    - New Views:
      - about
      - soon (coming soon)
    - More images:
      - simple logo simple/dark
      - text logo
    - Added Nav and Footer
    - General improvements

--- avaline-web/server.js
+++ avaline-web/server.js
@@ -1,31 +1,50 @@
 const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const config = require(path.join(__dirname, "../config.json"));
 const app = express()
-
-var inviteBot = "https://discordapp.com/api/oauth2/authorize?client_id=450754650417659916&permissions=8&scope=bot"
-var inviteServer = "https://discord.gg/NpWC4F4"
-
-var webVersion = "0.0.1"
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
-  layoutsDir: path.join(__dirname, '/views/layouts')
+  layoutsDir: path.join(__dirname, '/views/layouts'),
 }))
+
+//For static files
+app.use('/', express.static(path.join(__dirname, "/assets/")));
+
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (request, response) => {
   response.render('home', {
-    version: webVersion,
-    inviteBot: inviteBot,
-    inviteServer: inviteServer
+    home: true,
+    user: config.user,
+    config: config,
+    href: config.href,
+    title: "Titlemk",
+    pathToCSS: path.join(config.server.route, config.server.pathToCSS),
+  })
+})
+app.get('/about', (request, response) => {
+  response.render('about', {
+    about: true,
+    user: config.user,
+    config: config,
+    href: config.href,
+    title: "Titlemk",
+    pathToCSS: path.join(config.server.route, config.server.pathToCSS),
+  })
+})
+app.get('/dashboard*', (request, response) => {
+  response.render('soon', {
+    dashboard: true,
+    user: config.user,
+    config: config,
+    href: config.href,
+    title: "Titlemk",
+    pathToCSS: path.join(config.server.route, config.server.pathToCSS),
   })
 })
 
-module.exports = {
-  port: function(port) {
-    app.listen(port)
-  },
-}
+app.listen(config.port);
